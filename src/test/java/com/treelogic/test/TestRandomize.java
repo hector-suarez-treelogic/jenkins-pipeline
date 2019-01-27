bbd932b8cb3b136e363977ebb84275013a98e60f
hector.suarez <hector.suarez@TLPORTATIL073>
2019-01-27 16:05:21 +0100

    Test de la aplicacion

---
+++ src/test/java/com/treelogic/test/TestRandomize.java
@@ -0,0 +1,27 @@
+package com.treelogic.test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+public class TestRandomize {
+
+	@Test
+	public void testRandom() {
+		if (randomNumberGenerator() < 2) {
+			fail("Test Fail");
+		}
+		System.out.println("Test Ok - ");
+	}
+
+	private double randomNumberGenerator()
+	{
+	    double rangeMin = 0.0f;
+	    double rangeMax = 1.0f;
+	    Random r = new Random();
+	    double createdRanNum = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+	    return(createdRanNum);
+	}
+}
